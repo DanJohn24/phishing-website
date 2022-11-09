6f15b95013e9cad10e1e320ec5d88bd266d2080d
Daniel Lee <19026845@brookes.ac.uk>
2022-11-09 14:23:43 +0000

    new file:   conn.php

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+    //remote db connection
+    $host = 'ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'
+    $db = 'ofjkxkxm5e5a4un3'
+    $user = 'ntjiuzf0x77foh47'
+    $pass = 'oa3tltoa3buucpt7'
+    $charset = 'utf8mb4'
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+?>
