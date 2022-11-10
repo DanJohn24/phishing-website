b87d1ef1fa8397704a3c097476f017fb15d08166
DanJohn24 <19026845@brookes.ac.uk>
2022-11-10 18:33:01 +0000

    modified:   css/stylesheet.css modified:   index.php

--- index.php
+++ index.php
@@ -5,17 +5,19 @@
     require_once 'db/conn.php'; 
 ?>
 
-<!DOCTYPE html>
-<form>
-    <div class="inputBox">
-        <input type="email" name="email" value="">
-        <label>Username</label>
-    </div>
-    <div class="inputBox">
-            <input type="text" name="text" value="">
-            <label>Password</label>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+</head>
+<body>
+    <form>
+        <div class="inputBox">
+            <input type="email" name="email" value="">
+            <label>Username</label>
         </div>
-    <input type="submit" name="sign-in" value="Sign In">
-</form>
-</HTML>
-    
+        <div class="inputBox">
+                <input type="text" name="text" value="">
+                <label>Password</label>
+            </div>
+        <input type="submit" name="sign-in" value="Sign In">
+    </form>
+</body>
