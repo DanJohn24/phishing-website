f5a30236433640b8ea5a94f6f570e07987b95158
DanJohn24 <19026845@brookes.ac.uk>
2022-11-11 12:49:27 +0000

    modified:   css/stylesheet.css modified:   index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body>
     <form>
         <div class="inputBox">
-            <input type="email" name="email" value="" placeholder="Enter Username">
+            <input type="email" name="email" value="" placeholder="Email or phone">
         </div>
         <div class = "SignInButton">
             <input type="submit" name="sign-in" value="Sign In">
