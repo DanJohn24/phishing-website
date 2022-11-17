3e32b08f03f06ec6c0c06f8bc04b76f834aca349
DanJohn24 <19026845@brookes.ac.uk>
2022-11-17 16:29:13 +0000

    modified:   db/conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,11 +1,10 @@
 <?php
-    //$servername = "eu-cdbr-west-03.cleardb.net";
+    $servername = "eu-cdbr-west-03.cleardb.net";
     $username = "b26340cf38f897";
     $password = "39651a53";
     
     // Create connection
-    $conn = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_5af6573f25bac00', $username, $password);
-    //$conn = new mysqli($servername, $username, $password, );
+    $conn = new mysqli($servername, $username, $password, );
     
     // Check connection
     if ($conn->connect_error) {
