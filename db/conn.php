66f95a82159e75e37d2665fa10211d0366b34ded
DanJohn24 <19026845@brookes.ac.uk>
2022-11-17 16:34:13 +0000

    modified:   added database test

--- db/conn.php
+++ db/conn.php
@@ -14,4 +14,15 @@
     $data = $conn->query('SHOW databases');
     echo "Connected successfully";
 
+
+    //test
+    $test_query = $conn->query("SHOW databases;");
+    $result = mysqli_query($link, $test_query);
+
+    $tblCnt = 0;
+    while($tbl = mysqli_fetch_array($result)) {
+         $tblCnt++;
+         echo $tbl[0]."<br />\n";
+    }
+
 ?>
