cdeee62d756d3023ddc1fad063c6d96fdd3347e9
DanJohn24 <19026845@brookes.ac.uk>
2022-11-17 16:45:18 +0000

    modified:   db/conn.php. Corrected password

--- db/conn.php
+++ db/conn.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "eu-cdbr-west-03.cleardb.net";
     $username = "b26340cf38f897";
-    $password = "39651a53";
+    $password = "1bb944b79fba1a8";
     $db = 'heroku_5af6573f25bac00';
     
     // Create connection
