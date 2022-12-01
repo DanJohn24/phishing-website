7adc37ad8995ebdff72c4c796a5b31fe0b566dbe
DanJohn24 <19026845@brookes.ac.uk>
2022-12-01 13:12:23 +0000

    modified:   index.php. Removed <form>

--- index.php
+++ index.php
@@ -5,25 +5,25 @@
     require_once 'db/conn.php'; 
 ?>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
 <script>
     $( "#sign_in_button" ).click(function() {
-        alert( "Handler for .click() called.");
+        window.alert( "Handler for .click() called.");
     });
 </script> 
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
 <link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
 
-<form>
-    <div class="inputBox">
-        <input type="email" name="email" value="" placeholder="Email or phone">
-    </div>
-    
-    <div class="inputBox">
-        <input type="password" name="email" value="" placeholder="Password">
-    </div>
-    
-    <div class = "SignInButton">
-        <input id = "sign_in_button" type="submit" name="sign_in_button" value="sign in"/>
-    </div>
-</form> 
+<div class="inputBox">
+    <input type="email" name="email" value="" placeholder="Email or phone">
+</div>
+
+<div class="inputBox">
+    <input type="password" name="email" value="" placeholder="Password">
+</div>
+
+<div class = "SignInButton">
+    <input id = "sign_in_button" type="button" name="sign_in_button" value="sign in"/>
+</div>
