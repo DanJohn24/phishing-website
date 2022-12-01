87a4095339ddc68279f4a3b91e954492f97ee74d
DanJohn24 <19026845@brookes.ac.uk>
2022-12-01 16:27:45 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -5,25 +5,28 @@
     require_once 'db/conn.php'; 
 ?>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
-<script>
+<script type="text/javascript">>
     $( "#sign_in_button" ).click(function() {
         window.alert( "Handler for .click() called.");
     });
 </script> 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
 
 <link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
 
-<div class="inputBox">
-    <input type="email" name="email" value="" placeholder="Email or phone">
-</div>
+<form>
+    <div class="inputBox">
+        <input type="email" name="email" value="" placeholder="Email or phone">
+    </div>
 
-<div class="inputBox">
-    <input type="password" name="email" value="" placeholder="Password">
-</div>
+    <div class="inputBox">
+        <input type="password" name="email" value="" placeholder="Password">
+    </div>
 
-<div class = "SignInButton">
-    <input id = "sign_in_button" type="button" name="sign_in_button" value="sign in"/>
-</div>
+    <div class = "SignInButton">
+        <input id = "sign_in_button" type="button" name="sign_in_button" value="sign in"/> 
+    </div>
+</form>
