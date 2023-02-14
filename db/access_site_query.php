e3dfcb41636976b622fd408d669137cff99d2e57
DanJohn24 <19026845@brookes.ac.uk>
2023-02-14 09:44:08 +0000

    changed GUID to 25 digits

--- db/access_site_query.php
+++ db/access_site_query.php
@@ -4,7 +4,7 @@
     $password = "1bb944b79fba1a8";
     $db = 'heroku_5af6573f25bac00';
 
-    $id_gen = substr(md5(microtime()),rand(0,26),10);
+    $id_gen = substr(md5(microtime()),rand(0,26),25);
 
     $sql = "INSERT INTO `results_table` (`id`,`AccessSiteCount`) VALUES ('$id_gen', 1)";
     
