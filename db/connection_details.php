6446bbad897ae996fe5491f241e2b794c8b21f0e
DanJohn24 <19026845@brookes.ac.uk>
2023-03-10 19:24:27 +0000

    updated connnection password

--- db/connection_details.php
+++ db/connection_details.php
@@ -1,6 +1,6 @@
 <?php
     define ("servername" , "eu-cdbr-west-03.cleardb.net");
     define ("username" , "b26340cf38f897");
-    define ("password" , "a814c0ae3c6de75");
+    define ("password" , "c8044a2d5031229");
     define ("db" , 'heroku_5af6573f25bac00');
 ?>
